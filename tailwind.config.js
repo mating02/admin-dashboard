acd20042468b989e84062147f57355c599366ba8
mat <mat@MyDevice>
2023-09-13 22:04:10 +0200

    created basic grid layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'container': '1fr 5fr',
+        'header' : '2fr 1fr',
+        'cards' : 'repeat(auto-fit, minmax(150px, 1fr))',
+        'main' : '3fr 1fr'
+      },
+      gridTemplateRows: {
+        '3mm': 'repeat(3, min-content)'
+      },
+    },
   },
   plugins: [],
 }
