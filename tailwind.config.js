a511f37d8b3b00847e4092036a34604e07fca49d
mat <mat@MyDevice>
2023-09-16 02:05:49 +0200

    edited design and flex problem, finished design of cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,21 @@ module.exports = {
       gridTemplateColumns: {
         'container': '1fr 5fr',
         'header' : '2fr 1fr',
-        'cards' : 'repeat(auto-fit, minmax(150px, 1fr))',
-        'main' : '3fr 1fr'
+        'cards' : 'repeat(auto-fit, minmax(18rem, 1fr))',
+        'main' : '3fr 1fr',
+        'card': '1fr 25fr'
       },
       gridTemplateRows: {
         '3mm': 'repeat(3, min-content)',
-        'header': 'repeat(2, min-content)'
+        'header': 'repeat(2, min-content)',
+        'container': 'min-content auto',
+        'main': 'min-content auto'
       },
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif']
+      },
+      width: {
+        'button': 'clamp(0, min-content, auto)'
       }
     },
   },
